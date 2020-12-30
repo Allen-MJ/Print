4f18e1ec2183c95d675b8007dcccc9f4287b008f
Allen <maojing644963131@126.com>
2020-12-31 00:24:55 +0800

    连接蓝牙

---
+++ app/src/main/java/cn/allen/print/App.java
@@ -0,0 +1,27 @@
+package cn.allen.print;
+
+import android.annotation.SuppressLint;
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Created by Administrator
+ *
+ * @author 猿史森林
+ *         Date: 2017/11/28
+ *         Class description:
+ */
+public class App extends Application {
+
+    private static Context mContext;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mContext = getApplicationContext();
+    }
+
+    public static Context getContext() {
+        return mContext;
+    }
+}
--- app/src/main/java/cn/allen/print/MainActivity.java
+++ app/src/main/java/cn/allen/print/MainActivity.java
@@ -1,14 +1,19 @@
 package cn.allen.print;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatImageView;
+import androidx.appcompat.widget.AppCompatTextView;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.PendingIntent;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
@@ -16,6 +21,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 
 import com.google.zxing.BarcodeFormat;
@@ -27,14 +33,102 @@ import static androidx.core.content.PermissionChecker.PERMISSION_GRANTED;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String	TAG	= "MainActivity";
     private String serialnumber;
     private static final int REQUEST_CODE = 0x004;
     private AppCompatImageView img;
+    private AppCompatTextView status;
+    /**
+     * 连接状态断开
+     */
+    private static final int CONN_STATE_DISCONN = 0x007;
+
+
+    /**
+     * 使用打印机指令错误
+     */
+    private static final int PRINTER_COMMAND_ERROR = 0x008;
+
+
+    /**
+     * ESC查询打印机实时状态指令
+     */
+    private byte[] esc = { 0x10, 0x04, 0x02 };
+
+
+    /**
+     * CPCL查询打印机实时状态指令
+     */
+    private byte[] cpcl = { 0x1b, 0x68 };
+
+
+    /**
+     * TSC查询打印机状态指令
+     */
+    private byte[] tsc = { 0x1b, '!', '?' };
+
+    private static final int	CONN_MOST_DEVICES	= 0x11;
+    private static final int	CONN_PRINTER		= 0x12;
+    private PendingIntent mPermissionIntent;
+
+    /**
+     * 判断打印机所使用指令是否是ESC指令
+     */
+    private int	id = 0;
+    private ThreadPool threadPool;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initUi();
+        addEvent();
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if ( resultCode == RESULT_OK ){
+            switch (requestCode){
+                /*蓝牙连接*/
+                case Constant.BLUETOOTH_REQUEST_CODE:
+                    closeport();
+                    /*获取蓝牙mac地址*/
+                    String macAddress = data.getStringExtra(BluetoothDeviceList.EXTRA_DEVICE_ADDRESS);
+                    /* 初始化话DeviceConnFactoryManager */
+                    new DeviceConnFactoryManager.Build()
+                            .setId(id)
+                            /* 设置连接方式 */
+                            .setConnMethod(DeviceConnFactoryManager.CONN_METHOD.BLUETOOTH)
+                            /* 设置连接的蓝牙mac地址 */
+                            .setMacAddress(macAddress)
+                            .build();
+                    /* 打开端口 */
+                    Log.d(TAG, "onActivityResult: 连接蓝牙" + id);
+                    threadPool = ThreadPool.getInstantiation();
+                    threadPool.addTask(new Runnable() {
+                        @Override
+                        public void run() {
+                            DeviceConnFactoryManager.getDeviceConnFactoryManagers()[id].openPort();
+                        }
+                    });
+
+                    break;
+                case CONN_MOST_DEVICES:
+                    id = data.getIntExtra( "id", -1 );
+                    if (DeviceConnFactoryManager.getDeviceConnFactoryManagers()[id] != null &&
+                            DeviceConnFactoryManager.getDeviceConnFactoryManagers()[id].getConnState()){
+                        status.setText( getString( R.string.str_conn_state_connected ) + "\n" + getConnDeviceInfo() );
+                    } else {
+                        status.setText( getString( R.string.str_conn_state_disconnect ) );
+                    }
+                    break;
+                }
+            }
+    }
+
+    private void initUi(){
         img = findViewById(R.id.scan_tv);
+        status = findViewById(R.id.connect_tv);
         if(ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH)!=PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.BLUETOOTH}, REQUEST_CODE);
             return;
@@ -45,12 +139,20 @@ public class MainActivity extends AppCompatActivity {
             Uri uri = intent.getData();
             if(uri!=null){
                 serialnumber = uri.getQueryParameter("serialnumber");
-                AlertDialog.Builder builder = new AlertDialog.Builder(this).setMessage("serialnumber:"+serialnumber);
-                builder.show();
+                creatScan();
             }
         }
-        creatScan();
     }
+
+    private void addEvent(){
+        findViewById(R.id.bluetooth_connect).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnBluetoothConn();
+            }
+        });
+    }
+
     @SuppressLint("MissingSuperCall")
     @Override
     public void onRequestPermissionsResult(int requestCode,
@@ -64,11 +166,9 @@ public class MainActivity extends AppCompatActivity {
                         Uri uri = intent.getData();
                         if(uri!=null){
                             serialnumber = uri.getQueryParameter("serialnumber");
-                            AlertDialog.Builder builder = new AlertDialog.Builder(this).setMessage("serialnumber:"+serialnumber);
-                            builder.show();
+                            creatScan();
                         }
                     }
-                    creatScan();
                 }else{
                     finish();
                 }
@@ -87,13 +187,50 @@ public class MainActivity extends AppCompatActivity {
 
     Bitmap bitmap;
     private void creatScan(){
+        if(TextUtils.isEmpty(serialnumber)){
+            AlertDialog.Builder builder = new AlertDialog.Builder(this).setMessage("条码号为空!");
+            builder.setPositiveButton("知道了", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                    finish();
+                }
+            }).show();
+            return;
+        }
         new Thread(new Runnable() {
             @Override
             public void run() {
-                bitmap = createBarcode(TextUtils.isEmpty(serialnumber)?"01123124":serialnumber);
+                bitmap = createBarcode(serialnumber);
                 handler.sendEmptyMessage(0);
             }
         }).start();
+    }
+
+    /**
+     * 重新连接回收上次连接的对象，避免内存泄漏
+     */
+    private void closeport(){
+        if ( DeviceConnFactoryManager.getDeviceConnFactoryManagers()[id] != null
+                &&DeviceConnFactoryManager.getDeviceConnFactoryManagers()[id].mPort != null ){
+            DeviceConnFactoryManager.getDeviceConnFactoryManagers()[id].reader.cancel();
+            DeviceConnFactoryManager.getDeviceConnFactoryManagers()[id].mPort.closePort();
+            DeviceConnFactoryManager.getDeviceConnFactoryManagers()[id].mPort = null;
+        }
+    }
+
+    private String getConnDeviceInfo()
+    {
+        String str = "";
+        DeviceConnFactoryManager deviceConnFactoryManager = DeviceConnFactoryManager.getDeviceConnFactoryManagers()[id];
+        if (deviceConnFactoryManager != null
+                && deviceConnFactoryManager.getConnState()){
+            if ("BLUETOOTH".equals( deviceConnFactoryManager.getConnMethod().toString())){
+                str	+= "BLUETOOTH\n";
+                str	+= "MacAddress: " + deviceConnFactoryManager.getMacAddress();
+            }
+        }
+        return(str);
     }
 
     @SuppressLint("HandlerLeak")
@@ -114,7 +251,7 @@ public class MainActivity extends AppCompatActivity {
     /**
      * 蓝牙连接
      */
-    public void btnBluetoothConn( View view ){
+    public void btnBluetoothConn(){
         startActivityForResult( new Intent( this, BluetoothDeviceList.class ), Constant.BLUETOOTH_REQUEST_CODE );
     }
 
